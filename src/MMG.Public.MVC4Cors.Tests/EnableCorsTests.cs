adc434399bffa35817f9c99194becd1232a2ae8b
greenb1 <greenb1@.(none)>
2016-03-03 14:54:06 -0500

    - Add testing framework

---
+++ src/MMG.Public.MVC4Cors.Tests/EnableCorsTests.cs
@@ -0,0 +1,13 @@
+﻿// *************************************************
+// MMG.Public.MVCCors.Tests.EnableCorsTests.cs
+// Last Modified: 03/03/2016 2:52 PM
+// Modified By: Green, Brett (greenb1)
+// *************************************************
+
+using NUnit.Framework;
+
+namespace MMG.Public.MVCCors.Tests
+{
+    [TestFixture]
+    public class EnableCorsTests {}
+}
