d1e4e32b89f215a4b2002809a7a1aabf7a6b305c
greenb1 <greenb1@.(none)>
2016-03-03 16:27:30 -0500

    - separate Headers static class into own file

--- src/MMG.Public.MVC4Cors/CorsEnabledAttribute.cs
+++ src/MMG.Public.MVC4Cors/CorsEnabledAttribute.cs
@@ -62,14 +62,4 @@ namespace MMG.Public.MVC4Cors
                 AllowedDomains.Add(allowedDomain);
         }
     }
-
-    public static class Headers
-    {
-        public static string Origin = "Origin";
-        public static string AccessControlRequestMethod = "Access-Control-Request-Method";
-        public static string AccessControlRequestHeaders = "Access-Control-Request-Headers";
-        public static string AccessControlAllowMethods = "Access-Control-Allow-Methods";
-        public static string AccessControlAllowHeaders = "Access-Control-Allow-Headers";
-        public static string AccessControlAllowOrigin = "Access-Control-Allow-Origin";
-    }
 }
---
+++ src/MMG.Public.MVC4Cors/Headers.cs
@@ -0,0 +1,12 @@
+namespace MMG.Public.MVC4Cors
+{
+    public static class Headers
+    {
+        public static string Origin = "Origin";
+        public static string AccessControlRequestMethod = "Access-Control-Request-Method";
+        public static string AccessControlRequestHeaders = "Access-Control-Request-Headers";
+        public static string AccessControlAllowMethods = "Access-Control-Allow-Methods";
+        public static string AccessControlAllowHeaders = "Access-Control-Allow-Headers";
+        public static string AccessControlAllowOrigin = "Access-Control-Allow-Origin";
+    }
+}
