f4f0a19f416fb8b5a5348a85b4677c6c66a55597
greenb1 <greenb1@.(none)>
2016-03-04 08:38:53 -0500

    - Add NuSpec info

--- src/MMG.Public.MVC4Cors/Properties/AssemblyInfo.cs
+++ src/MMG.Public.MVC4Cors/Properties/AssemblyInfo.cs
@@ -1,25 +1,33 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// *************************************************
+// MMG.Public.MVC4Cors.AssemblyInfo.cs
+// Last Modified: 03/04/2016 8:31 AM
+// Modified By: Green, Brett (greenb1)
+// *************************************************
+
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("MMG.Public.MVC4Cors")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("MVC4 Action Filter to support Cors lockdown to a set of allowable domains")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ketchum Inc")]
+[assembly: AssemblyCompany("MMG")]
 [assembly: AssemblyProduct("MMG.Public.MVC4Cors")]
-[assembly: AssemblyCopyright("Copyright © Ketchum Inc 2016")]
+[assembly: AssemblyCopyright("Copyright © MMG 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("16b94622-1271-4ce2-af69-90c9cb35cb87")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +40,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
