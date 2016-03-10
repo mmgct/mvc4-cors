03b8a1e5aa63ced00ff4ecd0e5920408818db20a
greenb1 <greenb1@.(none)>
2016-03-10 13:05:37 -0500

    - Test page and controller actions for examining the CORS attribute behavior

--- src/MMG.Public.MVC4Cors.Web/Controllers/HomeController.cs
+++ src/MMG.Public.MVC4Cors.Web/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 ﻿// *************************************************
 // MMG.Public.MVC4Cors.Web.HomeController.cs
-// Last Modified: 03/10/2016 11:26 AM
+// Last Modified: 03/10/2016 12:46 PM
 // Modified By: Green, Brett (greenb1)
 // *************************************************
 
@@ -15,7 +15,14 @@ namespace MMG.Public.MVC4Cors.Web.Controllers
             return View();
         }
 
+        [CorsEnabled("http://localhost:62628, http://MMG-GREENB3:62628")]
         public JsonResult TestCors()
+        {
+            return Json("Result: true", JsonRequestBehavior.AllowGet);
+        }
+
+        [CorsEnabled("http://MMG-GREENB3:62628")]
+        public JsonResult TestCorsBlocked()
         {
             return Json("Result: true", JsonRequestBehavior.AllowGet);
         }
